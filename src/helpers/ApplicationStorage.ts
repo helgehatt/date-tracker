5fc1479dfe844f1420bbee5545643562ab2a815c
Helge Hatteland <helge.hatteland@gmail.com>
2022-12-28 00:11:08 +0100

    Replaced SecureStore with AsyncStorage

--- src/helpers/ApplicationStorage.ts
+++ src/helpers/ApplicationStorage.ts
@@ -1,17 +1,17 @@
-import * as SecureStore from "expo-secure-store";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 class ApplicationStorage {
   static SELECTED_DATES = "selected-dates";
 
   static async loadSelectedDates(): Promise<number[]> {
-    return SecureStore.getItemAsync(ApplicationStorage.SELECTED_DATES)
+    return AsyncStorage.getItem(ApplicationStorage.SELECTED_DATES)
       .catch(console.warn)
       .then((value) => value ?? "[]")
       .then(JSON.parse);
   }
 
   static async saveSelectedDates(dates: number[]) {
-    return SecureStore.setItemAsync(
+    return AsyncStorage.setItem(
       ApplicationStorage.SELECTED_DATES,
       JSON.stringify(dates)
     ).catch(console.warn);
