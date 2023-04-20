6d6e1b3b8d2eb8aa5d697d677ceb9e096f8bbb37
helgehatt <github.xpyo6@aleeas.com>
2023-04-20 23:28:13 +0200

    Updated MonthGenerator

--- src/helpers/MonthGenerator.ts
+++ src/helpers/MonthGenerator.ts
@@ -1,31 +1,36 @@
 type YearMonth = { year: number; month: number };
 
 class MonthGenerator {
-  current: YearMonth;
-  prev: () => YearMonth;
-  next: () => YearMonth;
+  private current: YearMonth;
+  private prevMonthGenerator: Generator<YearMonth, void>;
+  private nextMonthGenerator: Generator<YearMonth, void>;
 
-  constructor() {
-    const { year, month } = new Date().getComponents();
+  constructor(today: Date = new Date()) {
+    const { year, month } = today.getComponents();
 
-    function* prev() {
+    function* prevMonthGeneratorFunction() {
       for (let i = 1; true; i++) {
         yield new Date(Date.UTC(year, month - i, 1)).getComponents();
       }
     }
 
-    function* next() {
+    function* nextMonthGeneratorFunction() {
       for (let i = 1; true; i++) {
         yield new Date(Date.UTC(year, month + i, 1)).getComponents();
       }
     }
 
-    const prevMonthGenerator = prev();
-    const nextMonthGenerator = next();
-
     this.current = { year, month };
-    this.prev = () => prevMonthGenerator.next().value!;
-    this.next = () => nextMonthGenerator.next().value!;
+    this.prevMonthGenerator = prevMonthGeneratorFunction();
+    this.nextMonthGenerator = nextMonthGeneratorFunction();
+  }
+
+  prev() {
+    return this.prevMonthGenerator.next().value!;
+  }
+
+  next() {
+    return this.nextMonthGenerator.next().value!;
   }
 
   init(prev: number, next: number) {
